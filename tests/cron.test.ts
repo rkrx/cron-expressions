b78623e7313aee6473879c06d6e9ed91016f7d24
Ron Kirschler <ron.kirschler@gmail.com>
2020-07-15 22:30:08 +0200

    Typo

--- tests/cron.test.ts
+++ tests/cron.test.ts
@@ -1,6 +1,6 @@
 import {assertEquals} from "https://deno.land/std/testing/asserts.ts";
 import {format} from "https://deno.land/x/date_fns/index.js";
-import {CronExpression} from "../src/Cron.ts";
+import {CronExpression} from "../src/cron.ts";
 
 const t = (expression: string, offsetDate: string, expectedResult: string) => {
     return () => {
